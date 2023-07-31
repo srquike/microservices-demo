ec6eda5332990f7c8cfa6628648ae74da0c435c4
srquike <srquike@outlook.com>
2023-07-31 16:16:34 -0600

    Add User DTO class

---
+++ MicroservicesDemo.Application/Features/Users/Common/UserDto.cs
@@ -0,0 +1,9 @@
+﻿namespace MicroservicesDemo.Application.Features.Users.Common
+{
+    public class UserDto
+    {
+        public Guid Id { get; set; }
+        public string? Name { get; set; }
+        public bool IsActivated { get; set; }
+    }
+}
